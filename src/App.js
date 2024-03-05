ea46687537fd32b1bcefcd37feb8b9fa4bbb17c4
AS-EBaiden <eunice.baiden@alliedsolutions.net>
2024-03-05 16:55:08 -0500

    installed affiinity packages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I just added yarn</p>
+        <p>I installed affinity stuff</p>
         <a
           className="App-link"
           href="https://reactjs.org"
